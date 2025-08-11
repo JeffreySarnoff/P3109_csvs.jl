62c3ef5fa957c488a4ad611d6cf555370283c0ae
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2025-08-11 02:20:04 -0400

    Update P3109_csvs.jl

--- src/P3109_csvs.jl
+++ src/P3109_csvs.jl
@@ -7,5 +7,16 @@ using CSV
 include("csv_dir_ok.jl")
 confirm_csv_dir()
 
+#=
+usage
+
+csvfile = csv_filepath(bits = 8, suffix = "se", base = 10)
+csvcodes, csvdata = csv_data(csvfile)
+
+println("encoding = ")
+println(csvcodes)
+
+=#
+
 
 end  # module
