e8465202534975c6bba812c0d92bd13b7ffd5247
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2025-08-11 01:45:42 -0400

    Update csv_dir_ok.jl

--- src/csv_dir_ok.jl
+++ src/csv_dir_ok.jl
@@ -1,3 +1,12 @@
 function confirm_csv_env()
-  csv_dir = ENV["P3109_CSV_DIR"]
+    csv_dir = get(ENV, "P3109_CSV_DIR", nothing)
+    if isnothing(csv_dir)
+        error("""ENV["P3109_CSV_DIR"] is not set""")
+    end
+    if !isdir(csv_dir)
+        error("""ENV["P3109_CSV_DIR"] $(csv_dir) is not a full directory path""")
+    end
+end
+
+
   
